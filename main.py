cdf1fd1ccc987844221b69e9bab357029301003d
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-01-30 21:08:45 +1100

    Changed code to make the light blink

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Turn the internal led on to show that the pico is turned on
 
 import machine
+import time
 
 # configure LED Pin as an output pin and create and led object for Pin class
+# then make the led blink in an infinite loop
 led = machine.Pin("LED", machine.Pin.OUT)
 
-led.on()
+while True:
+    led.on()
+    time.sleep(1)
+    led.off()
+    time.sleep(1)
