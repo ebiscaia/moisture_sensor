73e5120fb184e45c458f37ea9f51aa47c7ce6f7e
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-01-30 21:30:08 +1100

    Make an external light on

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# Turn the internal led on to show that the pico is turned on
+# Turn an external led to show that the Pico is powered on
 
 import machine
-import time
 
 # configure LED Pin as an output pin and create and led object for Pin class
 # then make the led blink in an infinite loop
-led = machine.Pin("LED", machine.Pin.OUT)
+led = machine.Pin(4, machine.Pin.OUT)
 
-while True:
-    led.on()
-    time.sleep(1)
-    led.off()
-    time.sleep(1)
+led.on()
