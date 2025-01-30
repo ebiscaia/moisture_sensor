e1fcdfa23aaa7967d53e20016c1a9ff45186820b
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-01-30 21:37:29 +1100

    Changed code to change led following a parameter

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# Turn an external led to show that the Pico is powered on
+# Set a value for power and turn one of the leds accordingly
 
 import machine
 
+power = 49
+
 # configure LED Pin as an output pin and create and led object for Pin class
 # then make the led blink in an infinite loop
-led = machine.Pin(4, machine.Pin.OUT)
+red = machine.Pin(4, machine.Pin.OUT)
+green = machine.Pin(5, machine.Pin.OUT)
 
-led.on()
+if power > 50:
+    light = green
+else:
+    light = red
+
+light.on()
