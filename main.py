d472f24a700f00bd9e610ab53d2af23b5c786244
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-02-05 19:28:43 +1100

    Added the ability to turn the board on

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from time import sleep
 
 # configure LED Pin as an output pin and create and led object for Pin class
 # then make the led blink in an infinite loop
+board = Pin("LED", Pin.OUT)
 red = Pin(4, Pin.OUT)
 green = Pin(5, Pin.OUT)
 
-adc_value = ADC(28)
+adc_moisture_value = ADC(27)
+adc_battery_value = ADC(28)
+conversion_factor = 3.3 / 65536
 
 
 def swapLights(light1, light2):
