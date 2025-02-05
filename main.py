0213bd8624facd471a765a787c34ba5a668ecae3
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-02-05 21:33:02 +1100

    Added lights to show battery status

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from time import sleep
 board = Pin("LED", Pin.OUT)
 red_moisture = Pin(4, Pin.OUT)
 green_moisture = Pin(5, Pin.OUT)
+red_battery = Pin(15, Pin.OUT)
+green_battery = Pin(14, Pin.OUT)
 
 adc_moisture_value = ADC(27)
 adc_battery_value = ADC(28)
@@ -27,6 +29,10 @@ while True:
         swapLights(red_moisture, green_moisture)
     else:
         swapLights(green_moisture, red_moisture)
+    if adc_battery_converted <= 1:
+        swapLights(red_battery, green_battery)
+    else:
+        swapLights(green_battery, red_battery)
 
     print(f"Moisture: {adc_moisture_converted} || Battery: {adc_battery_converted}")
     sleep(10)
