c3e35b0c27c9ab1931bb7953bd79f561441f0e53
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2025-02-16 18:16:10 +1100

    Changes to pass values as percentages to HomeAssistant

--- main.py
+++ main.py
@@ -40,6 +40,17 @@ def connectMQTT(client, broker, port, user, passwd):
     return client
 
 
+def correctThreshold(value, threshold):
+    if value > threshold:
+        return threshold
+    return value
+
+
+def convertToPerc(value, threshold):
+    value = correctThreshold(value, threshold)
+    return value / threshold * 100
+
+
 # configure LED Pin as an output pin and create and led object for Pin class
 # then make the led blink in an infinite loop
 board = Pin("LED", Pin.OUT)
@@ -49,7 +60,9 @@ red_battery = Pin(15, Pin.OUT)
 green_battery = Pin(14, Pin.OUT)
 
 adc_moisture_value = ADC(27)
+adc_moisture_max = 2
 adc_battery_value = ADC(26)
+adc_battery_max = 3
 conversion_factor = 3.3 / 65536
 
 wifi_config = loadJson("wifi.json")
@@ -75,11 +88,15 @@ while True:
     board.on()
     adc_moisture_converted = 3.3 - adc_moisture_value.read_u16() * conversion_factor
     adc_battery_converted = adc_battery_value.read_u16() * conversion_factor
+
     checkCondition(adc_moisture_converted, 0.8, green_moisture, red_moisture)
     checkCondition(adc_battery_converted, 2, green_battery, red_battery)
 
+    adc_moisture_perc = convertToPerc(adc_moisture_converted, adc_moisture_max)
+    adc_battery_perc = convertToPerc(adc_battery_converted, adc_battery_max)
+
     print(f"Moisture: {adc_moisture_converted} || Battery: {adc_battery_converted}")
-    mqtt_client.publish(f"{mqtt_topic}/moisture", str(adc_moisture_converted))
-    mqtt_client.publish(f"{mqtt_topic}/battery", str(adc_battery_converted))
+    mqtt_client.publish(f"{mqtt_topic}/moisture", str(adc_moisture_perc))
+    mqtt_client.publish(f"{mqtt_topic}/battery", str(adc_battery_perc))
 
     sleep(10)
